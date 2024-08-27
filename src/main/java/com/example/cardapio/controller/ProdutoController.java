083bf839252f65211312741b830e3552a653d825
VictorLapaDev <victorlucaslapa@gmail.com>
2024-08-27 11:04:35 -0300

    feat:Criando metodos getAll e addProduto

--- src/main/java/com/example/cardapio/controller/ProdutoController.java
+++ src/main/java/com/example/cardapio/controller/ProdutoController.java
@@ -1,4 +1,30 @@
 package com.example.cardapio.controller;
 
+import com.example.cardapio.modal.Produto;
+import com.example.cardapio.repository.ProdutoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/produto")
 public class ProdutoController {
+
+    @Autowired
+    ProdutoRepository repository;
+
+    @GetMapping
+    public List<Produto> getAll() {
+        return repository.findAll();
+    }
+
+
+
+    @PostMapping
+    public Produto addProduto(@RequestBody Produto param){
+        return repository.save(param);
+    }
+
 }
