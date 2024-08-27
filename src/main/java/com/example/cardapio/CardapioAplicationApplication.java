53896550db955b94225a1cb2bea8fb3e106bb456
VictorLapaDev <victorlucaslapa@gmail.com>
2024-08-27 09:57:18 -0300

    feat:project

---
+++ src/main/java/com/example/cardapio/CardapioAplicationApplication.java
@@ -0,0 +1,13 @@
+package com.example.cardapio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardapioAplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardapioAplicationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cardapio/CardapioAplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cardapio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardapioAplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
