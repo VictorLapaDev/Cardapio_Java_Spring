fd4b325d5585de1e9b22b2cd48e3d87f898ac71e
VictorLapaDev <victorlucaslapa@gmail.com>
2024-08-27 12:56:26 -0300

    feat: Controller finalizada, metodos update, delete, findById

--- src/main/java/com/example/cardapio/controller/ProdutoController.java
+++ src/main/java/com/example/cardapio/controller/ProdutoController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 
 @RestController
@@ -15,9 +16,15 @@ public class ProdutoController {
     @Autowired
     ProdutoRepository repository;
 
+
     @GetMapping
     public List<Produto> getAll() {
         return repository.findAll();
+    }
+
+    @GetMapping("{id}")
+    public Optional<Produto> getById(@PathVariable Long id){
+        return repository.findById(id);
     }
 
 
@@ -26,5 +33,29 @@ public class ProdutoController {
     public Produto addProduto(@RequestBody Produto param){
         return repository.save(param);
     }
+
+
+
+
+    @PutMapping("{id}")
+    public Produto updateProduto(@PathVariable Long id, @RequestBody Produto param){
+
+        Produto newProduto = repository.findById(id).orElseThrow(() -> new RuntimeException("not found"));
+
+        newProduto.setTitulo(param.getTitulo());
+        newProduto.setPreco(param.getPreco());
+        newProduto.setImage(param.getImage());
+
+        return  repository.save(newProduto);
+    }
+
+
+
+    @DeleteMapping("{id}")
+    public void deleteProduto(@PathVariable Long id){
+        repository.deleteById(id);
+    }
+
+
 
 }
