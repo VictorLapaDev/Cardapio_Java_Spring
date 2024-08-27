363762c7d67eb063852f46e8c2f3199533463dfe
VictorLapaDev <victorlucaslapa@gmail.com>
2024-08-27 10:12:59 -0300

    feat:Entidade criada

--- src/main/java/com/example/cardapio/modal/Produto.java
+++ src/main/java/com/example/cardapio/modal/Produto.java
@@ -1,4 +1,47 @@
 package com.example.cardapio.modal;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "produtos")
 public class Produto {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String titulo;
+    private int preco;
+    private String image;
+
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public int getPreco() {
+        return preco;
+    }
+
+    public void setPreco(int preco) {
+        this.preco = preco;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
 }
